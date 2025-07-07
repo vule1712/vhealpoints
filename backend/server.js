9b310b1a4615779eb684db564878154016ba27e3
Vu Le Phuong Anh <vulephuonganh@gmail.com>
2025-07-07 22:11:37 +0700

    deployment: Update server.js to fix CORS issue

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,17 @@ const io = initSocket(server);
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']; // Add frontend URL
+const allowedOrigins = [
+        'http://localhost:5173',
+        'https://vhealpoints.vercel.app'
+    ];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cors({
+    origin: allowedOrigins, 
+    credentials: true
+}));
 
 app.use((req, res, next) => {
     req.io = getIO();
