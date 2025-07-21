ffde9d66d9e3ca45dc5e0ae3008f811b81fe4d20
Vu Le Phuong Anh <vulephuonganh@gmail.com>
2025-07-21 22:17:30 +0700

    update websocket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_API_URL;
 export const socket = io(URL, {
     autoConnect: false,
+    transports: ['websocket'],
     auth: {
         userId: null
     }
